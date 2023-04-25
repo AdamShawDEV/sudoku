af3725d256b9338f6487e664659907f36ad690a2
Sitingduck <47095718+AdamShawDEV@users.noreply.github.com>
2023-04-25 18:48:21 +0700

    added menu component

---
+++ src/components/Menu.js
@@ -0,0 +1,15 @@
+import styles from "./modules/Menu.module.css";
+
+function Menu() {
+  return (
+    <div>
+      <h1>sudoku</h1>
+      <span>new game:</span>
+      <button>easy</button>
+      <button>medium</button>
+      <button>hard</button>
+    </div>
+  );
+}
+
+export default Menu;
